e0107ddfae83075c14e1ab4676444180c9278729
smjxf <77221005+smjxf@users.noreply.github.com>
2023-03-03 13:31:59 +0800

    更新

--- JS/js/18/18av.js
+++ JS/js/18/18av.js
@@ -1,13 +1,13 @@
 var rule = {
     title:'18av',
-    host:'https://maa1812.com',
+    host:'https://maa1815.com/',
     url:'/zh/fyclass/all/fypage.html',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'最近更新',//静态分类名称拼接
-    class_url:'chinese_list',//静态分类标识拼接
+    class_name: '中文字幕AV&国产专区&欧美精品&动漫&中文字幕&人气女忧&不雅视频&韩三级&热剧大人版&抖音妹合集&猫咪自拍',
+    class_url: 'chinese_random&32&16&31&34&35&37&39&40&41&42',
    //class_parse:'ul.animenu__nav&&li;a&&Text;a&&href',
     limit:5,
     play_parse:true,
