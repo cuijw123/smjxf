5924cffed61e988fabf449ab74789622befef43f
smjxf <77221005+smjxf@users.noreply.github.com>
2023-03-17 12:44:41 +0800

    更新

--- TV/直播.js
+++ TV/直播.js
@@ -4,8 +4,8 @@
 {"name": "星辰",     "url": "http://tvmvip.com/xclive.txt"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
 {"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
-{"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
+{"name": "肥猫",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
 {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
 {"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
-{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt"}
+{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt&"}
 ]
